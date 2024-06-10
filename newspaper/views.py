ece3a09cc0aaa0fbd9b5d1c3617bd32eae49d136
Serhii Khodko <sergeyhodko123@gmail.com>
2024-06-10 03:54:54 +0300

    created home page view, updated path

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from newspaper.models import Redactor, Topic, Newspaper
+
+
+def index(request):
+    redactors = Redactor.objects.all()
+    topics = Topic.objects.all()
+    newspapers = Newspaper.objects.all()
+    context = {
+        "redactors": redactors,
+        "newspapers": newspapers,
+        "topics": topics
+    }
+    return render(request, "newspaper/index.html", context=context)
