41eca596f685e483be0791104189abef28c8f2e0
Serhii Khodko <sergeyhodko123@gmail.com>
2024-06-10 03:38:33 +0300

    registered models in admin.py

--- newspaper/admin.py
+++ newspaper/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from newspaper.models import Redactor, Topic, Newspaper
+
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["name",]
+    list_filter = ["name",]
+    search_fields = ["name",]
+
+
+@admin.register(Redactor)
+class RedactorAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("years_of_experience",)
+    fieldsets = UserAdmin.fieldsets + (("Important data", {"fields": ("years_of_experience",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((
+                                                   "Important data", {
+                                                       "fields": (
+                                                           "years_of_experience",
+                                                           "first_name",
+                                                           "last_name",
+                                                       )
+                                                   }
+                                               ),)
+
+
+@admin.register(Newspaper)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ["title", "topic", "published_date",]
+    list_filter = ["published_date",]
+    search_fields = ["name",]
